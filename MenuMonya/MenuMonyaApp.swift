1c235bddc98354097574d02e99a25430ddda1c8c
KSYong <ericyong95@gmail.com>
2023-04-21 16:48:33 +0900

    Initial Commit

---
+++ MenuMonya/MenuMonyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMonyaApp.swift
+//  MenuMonya
+//
+//  Created by 권승용 on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MenuMonyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
