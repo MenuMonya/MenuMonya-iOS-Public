907d87670113a0429cec7ddc344d6f873b88e8a2
Kwon Seung Yong <ericyong95@gmail.com>
2023-04-21 17:01:20 +0900

    [feat] : Initial commit

---
+++ MenuMonya/Views/MainView/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  MenuMonya
+//
+//  Created by 권승용 on 2023/04/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
