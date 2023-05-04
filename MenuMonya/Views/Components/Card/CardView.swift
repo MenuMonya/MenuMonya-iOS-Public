8931761b6971b7f5495276534f6a910529034d7e
KSYong <ericyong95@gmail.com>
2023-05-04 18:42:42 +0900

    [feat] : UI 사이즈 변경

--- MenuMonya/Views/Components/Card/CardView.swift
+++ MenuMonya/Views/Components/Card/CardView.swift
@@ -13,47 +13,57 @@ struct CardView: View {
     @Binding var isShowingMenuDetail: Bool
     
     var body: some View {
-        VStack {
-            Spacer()
-            VStack(spacing: 0) {
-                if !(card.menu.date[viewModel.currentDateString]?["main"]?.isEmpty ?? true) {
-                    menus()
-                }
-                details()
+        VStack(spacing: 0) {
+            if !(card.menu.date[viewModel.currentDateString]?["main"]?.isEmpty ?? true) {
+                menus()
+            } else {
+                menuReportLink()
+                    .padding(.top, 10)
             }
-            .frame(width: UIScreen.main.bounds.width - 28)
-            .background(Color("background.cardview"))
-            .cornerRadius(10)
-            .shadow(radius: 1)
+            details()
+                .padding(.vertical, 10)
         }
-        .frame(maxHeight: 240)
+        .padding(.horizontal, 10)
+        .frame(width: UIScreen.main.bounds.width - 28)
+        .background(Color("background.cardview"))
+        .cornerRadius(10)
+        .shadow(radius: 1)
     }
     
     @ViewBuilder
     func menus() -> some View {
-        VStack {
-            HStack(spacing: 0) {
+        VStack(spacing: 0) {
+            HStack(alignment: .top, spacing: 0) {
                 Text("메인 메뉴")
                     .font(.pretendard(.semiBold, size: 12))
                     .foregroundColor(Color("primary.orange"))
                     .frame(width: 70, alignment: .leading)
+                    .padding(.bottom, 24)
+                    .padding(.top, 10)
                 Text((card.menu.date[viewModel.currentDateString]?["main"] ?? Menu.dummy.date[viewModel.currentDateString]!["main"])!)
                     .font(.pretendard(.regular, size: 12))
                     .foregroundColor(Color("dark_1"))
                     .lineLimit(2)
+                    .lineSpacing(6)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.top, 10)
                 Spacer()
             }
-            .padding(.top, 10)
             CustomDivider(color: Color("grey_200"))
-            HStack(spacing: 0) {
+            HStack(alignment: .top, spacing: 0) {
                 Text("사이드 메뉴")
                     .font(.pretendard(.semiBold, size: 12))
                     .foregroundColor(Color("primary.orange"))
                     .frame(width: 70, alignment: .leading)
+                    .padding(.bottom, 24)
+                    .padding(.top, 6)
                 Text((card.menu.date[viewModel.currentDateString]?["side"] ?? Menu.dummy.date[viewModel.currentDateString]!["side"])!)
                     .font(.pretendard(.regular, size: 12))
                     .foregroundColor(Color("dark_1"))
-                    .lineLimit(1)
+                    .lineLimit(2)
+                    .lineSpacing(6)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.top, 6)
                 Spacer()
             }
             CustomDivider(color: Color("grey_200"))
@@ -62,6 +72,7 @@ struct CardView: View {
                     .font(.pretendard(.semiBold, size: 12))
                     .foregroundColor(Color("primary.orange"))
                     .frame(width: 70, alignment: .leading)
+                    .padding(.vertical, 6)
                 Text((card.menu.date[viewModel.currentDateString]?["dessert"] ?? Menu.dummy.date[viewModel.currentDateString]!["dessert"])!)
                     .font(.pretendard(.regular, size: 12))
                     .foregroundColor(Color("dark_1"))
@@ -70,72 +81,81 @@ struct CardView: View {
             }
             CustomDivider(color: Color("grey_200"))
         }
-        .padding(.horizontal, 10)
+    }
+    
+    @ViewBuilder
+    func menuReportLink() -> some View {
+        VStack(spacing: 0) {
+            CustomDivider(color: Color("grey_200"))
+            Link(destination: URL(string: "https://open.kakao.com/o/gKPs3pif")!) {
+                Image("button.reportMenu")
+            }
+            .padding(.vertical, 25)
+            CustomDivider(color: Color("grey_200"))
+            Text("메뉴는 매일 11시에 업데이트됩니다.")
+                .font(.pretendard(.regular, size: 12))
+                .padding(.vertical, 6)
+            CustomDivider(color: Color("grey_200"))
+        }
     }
     
     @ViewBuilder
     func details() -> some View {
-        HStack(spacing: 8) {
-            VStack(spacing: 4) {
-                HStack {
-                    Text(card.restaurant.name)
-                        .font(.pretendard(.semiBold, size: 14))
-                        .foregroundColor(Color("grey_900"))
-                        .padding(.top, 8)
-                    Spacer()
-                }
-                .padding(.bottom, 3)
-                HStack {
-                    Text("가격")
-                        .font(.pretendard(.semiBold, size: 12))
-                        .foregroundColor(Color("grey_900"))
-                    Spacer()
-                    Text(card.restaurant.price.cardPrice)
-                        .font(.pretendard(.regular, size: 12))
-                        .foregroundColor(Color("grey_900"))
-                }
-                HStack {
-                    Text("점심 운영시간")
-                        .font(.pretendard(.semiBold, size: 12))
-                        .foregroundColor(Color("grey_900"))
-                    Spacer()
-                    Text("\(card.restaurant.time.openTime)~\(card.restaurant.time.closeTime)")
-                        .font(.pretendard(.regular, size: 12))
-                        .foregroundColor(Color("grey_900"))
-                }
-                HStack {
-                    Text("전화번호")
-                        .font(.pretendard(.semiBold, size: 12))
-                        .foregroundColor(Color("grey_900"))
-                    Spacer()
-                    Text(card.restaurant.phoneNumber)
-                        .font(.pretendard(.regular, size: 12))
-                        .foregroundColor(Color("grey_900"))
-                }
-                HStack {
-                    Text("위치")
-                        .font(.pretendard(.semiBold, size: 12))
-                        .foregroundColor(Color("grey_900"))
-                    Spacer()
-                    Text(card.restaurant.location.description)
-                        .font(.pretendard(.regular, size: 12))
-                        .foregroundColor(Color("grey_900"))
+        VStack(spacing: 5) {
+            HStack(alignment: .top, spacing: 0) {
+                Text(card.restaurant.name)
+                    .font(.pretendard(.semiBold, size: 14))
+                    .foregroundColor(Color("grey_900"))
+                Spacer()
+                if !(card.menu.date[viewModel.currentDateString]?["main"]?.isEmpty ?? true) {
+                    Button {
+                        /* TODO - 자세히 보기 -> 가게 모달 생성? 또는 카드뷰 자체 탭 시 가게 모달 생성 */
+                        isShowingMenuDetail = true
+                    } label: {
+                        Text("자세히 보기")
+                            .font(.pretendard(.semiBold, size: 10))
+                            .underline()
+                            .foregroundColor(Color("grey_500"))
+                    }
                 }
             }
-            VStack(alignment: .trailing, spacing: 8) {
-                HStack {
-                    Spacer()
-                    if !(card.menu.date[viewModel.currentDateString]?["main"]?.isEmpty ?? true) {
-                        Button {
-                            /* TODO - 자세히 보기 -> 가게 모달 생성? 또는 카드뷰 자체 탭 시 가게 모달 생성 */
-                            isShowingMenuDetail = true
-                        } label: {
-                            Text("자세히 보기")
-                                .font(.pretendard(.semiBold, size: 10))
-                                .underline()
-                                .foregroundColor(Color("grey_500"))
-                        }
-                        .padding(.top, 5)
+            HStack(spacing: 8) {
+                VStack(spacing: 4) {
+                    HStack(spacing: 0) {
+                        Text("가격")
+                            .font(.pretendard(.semiBold, size: 12))
+                            .foregroundColor(Color("grey_900"))
+                        Spacer()
+                        Text("\(card.restaurant.price.cardPrice)원")
+                            .font(.pretendard(.regular, size: 10))
+                            .foregroundColor(Color("grey_900"))
+                    }
+                    HStack {
+                        Text("점심 운영시간")
+                            .font(.pretendard(.semiBold, size: 12))
+                            .foregroundColor(Color("grey_900"))
+                        Spacer()
+                        Text("\(card.restaurant.time.openTime)~\(card.restaurant.time.closeTime)")
+                            .font(.pretendard(.regular, size: 10))
+                            .foregroundColor(Color("grey_900"))
+                    }
+                    HStack {
+                        Text("전화번호")
+                            .font(.pretendard(.semiBold, size: 12))
+                            .foregroundColor(Color("grey_900"))
+                        Spacer()
+                        Text(card.restaurant.phoneNumber)
+                            .font(.pretendard(.regular, size: 10))
+                            .foregroundColor(Color("grey_900"))
+                    }
+                    HStack {
+                        Text("위치")
+                            .font(.pretendard(.semiBold, size: 12))
+                            .foregroundColor(Color("grey_900"))
+                        Spacer()
+                        Text(card.restaurant.location.description)
+                            .font(.pretendard(.regular, size: 10))
+                            .foregroundColor(Color("grey_900"))
                     }
                 }
                 AsyncImage(url: URL(string: card.restaurant.imgUrl)) { image in
@@ -143,15 +163,13 @@ struct CardView: View {
                         .resizable()
                         .scaledToFit()
                         .frame(width: 66, height: 66)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
                 } placeholder: {
                     ProgressView()
                         .frame(width: 66, height: 66)
                 }
             }
-            .frame(width: 66)
         }
-        .padding(.horizontal, 10)
-        .padding(.bottom, 10)
     }
 }
 
